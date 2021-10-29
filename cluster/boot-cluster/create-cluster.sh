809823d0aa6517a0f4d23b12b983b5909b8aee25
Leif <leifl@kth.se>
2021-10-29 15:42:28 +0200

    new secret

--- cluster/boot-cluster/create-cluster.sh
+++ cluster/boot-cluster/create-cluster.sh
@@ -3,8 +3,7 @@
 # Create the kind cluster.
 kind create cluster --config=kind-cluster.yaml
 
-# Install flux to the cluster and specify the github repo that flux will monitor. Note
-# that the repository specified here contains only the flux system.
+# Install flux to the cluster and specify the github repo that flux will monitor.
 flux bootstrap github \
  --owner=$GITHUB_USER \
  --repository=learn-ms \
